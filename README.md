c334ea11bd609e742d6af342912d416c53086869
PavelZaika <zpk.loki@gmail.com>
2021-11-29 15:55:19 +0300

    part4lesson14

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 Udemy. "Python разработка - с нуля до профессионала. Python 3" YouRa Allakhverdov https://www.udemy.com/course/python-python-3/
 
 Раздел 4. Основы Python.
+-- part4.py
--- part4.py
+++ part4.py
@@ -1,1 +1,24 @@
+# Part - 4
+# Lesson - 10
+
 print('Hello World!')
+
+
+# Part - 4
+# Lesson - 12
+
+print(2 ** 3)
+print(2345 % 2)
+
+# Part - 4
+# Lesson - 13
+
+x = 5
+type_of_var = type(x)
+print(type_of_var)
+
+if type_of_var == int:
+    print('done')
+
+# Part - 4
+# Lesson - 14
