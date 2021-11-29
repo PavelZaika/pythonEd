d21aec4a1c829291c3f78d3ea6b62a5d5e5f2adf
PavelZaika <zpk.loki@gmail.com>
2021-11-29 13:04:58 +0300

    testOffice

--- helloWorld.py
+++ helloWorld.py
@@ -5,12 +5,18 @@ print(2 * 4)
 
 
 def test():
-    print(test2)
+    print (test2)
 
 
 def testNew():
-    print(test1)
+    print (test1)
 
 
+def test3(a, b):
+    print (a + b)
+    print ('Res')
+
+
+test3(test1, test2)
 test()
 testNew()
