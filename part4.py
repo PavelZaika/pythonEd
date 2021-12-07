59235a57b7b0c9e6f0773da6f400433c5d8bedc4
PavelZaika <zpk.loki@gmail.com>
2021-12-07 12:43:56 +0300

    some_coments

--- part4.py
+++ part4.py
@@ -23,20 +23,20 @@ if type_of_var == int:
 # Part - 4
 # Lesson - 15
 
-greeting = 'Hello Python!'
+greeting = '0123456789!'
 greeting_length = len(greeting)
-print(greeting_length)
-print(greeting[0])
-print(greeting[2:5])
-print(greeting[6:10])
-print(greeting[-5:-2])
-print(greeting[2:])
-print(greeting[:5])
-print(greeting[:])
-print(greeting[::2])
-print(greeting[1::3])
-print(greeting[1:9:3])
-print(greeting[::-1])
+print(greeting_length)  # длина
+print(greeting[0])  # первый символ
+print(greeting[2:5])  # с 2 по 5(5 не включает)
+print(greeting[6:10])  # с 6 по 9 включительно
+print(greeting[-5:-2])  # с конца с пятого символа до второго с конца
+print(greeting[2:])  # с третьего символа до конца
+print(greeting[:5])  # с первого до 5(с нулевого до 4 включительно)
+print(greeting[:])  # все
+print(greeting[::2])  # с первого до конца каждые 2
+print(greeting[1::3])  # с первого до конца каждый третий
+print(greeting[1:9:3])  # с первого символа по 9 через 3 символа
+print(greeting[::-1])  # Развернуть строку
 
 # Part - 4
 # Lesson - 16
