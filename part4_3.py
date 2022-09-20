59cac8cdcf8b3a653d7c6b88e0534603786b20a8
PavelZaika <zpk.loki@gmail.com>
2022-09-20 17:36:55 +0300

    Logical Operators

--- part4_3.py
+++ part4_3.py
@@ -89,4 +89,34 @@ print('Access is permitted: ' + str(age >= 18))
 # Part - 4
 # Lesson - 24
 
-# Логические операторы
+# Logical Operators
+
+x = 1
+y = 2
+
+# print(x > 1)
+# print(y > 1)
+
+# and, or, not
+# print(x > 1 and y > 1)
+# print(x > 1 or y > 1)
+# print(not x > 1)
+# print(not y > 1)
+
+print(True and True)
+print(True or True)
+print(not True)
+
+print(False and False)
+print(False or False)
+print(not False)
+
+print(True and False)
+print(True or False)
+
+name = 'John'
+age = 13
+is_married = False
+
+if age > 18 or is_married == False:
+    print('Hi {}! You can find a girl of your dream here!'.format(name))
